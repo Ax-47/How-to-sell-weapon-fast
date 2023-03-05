33e9f5cd8d9b2077958f68a63b3f14a9531bd8ef
Ax-47 <armingLwnza@gmail.com>
2023-03-05 08:23:14 +0700

    as

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 // product
 Route::post('/product', [ProductController::class,'CreateProduct'])->middleware("auth");   //
 Route::get('/product', [ProductController::class,'CreateProductView'])->middleware("auth");//
-Route::get('/product/{id}', [ProductController::class,'ProductView']);
+Route::get('/product/{id}', [ProductController::class,'ProductView']);                     //
 Route::put('/product/{id}/edit')->middleware("auth");
 Route::delete('/product/{id}/delete')->middleware("auth");
 // Auth
